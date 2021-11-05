c2ed09bbf56ff5c404c8171168eca6b4f058ba0b
StarHeartHunt <starheart233@gmail.com>
2021-11-05 23:11:58 +0800

    feat: implement item demand router

--- controller/widget/item.go
+++ controller/widget/item.go
@@ -1,11 +1,26 @@
 package widget
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func ItemDemand(c echo.Context) error {
-	return c.String(http.StatusOK, c.Param("itemName"))
+	bytes, err := ioutil.ReadFile("data/item_demand.json")
+	if err != nil {
+		fmt.Println("读取json文件失败", err)
+		return err
+	}
+	var m map[string]interface{}
+	err = json.Unmarshal(bytes, &m)
+	if err != nil {
+		fmt.Println("解析数据失败", err)
+		return err
+	}
+
+	return c.JSON(http.StatusOK, m[c.Param("itemName")])
 }
